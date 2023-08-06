16356e02ed28b79945f3d7d27f802c675292f2a2
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-06 13:07:06 +0800

    progress of Roosevelt_madlibs

--- Projects/madlibs.py
+++
@@ -1,9 +0,0 @@
-# Project idea by Kylie Ying on freeCodeCamp: https://www.youtube.com/watch?v=8ext9G7xspg
-
-#string concatenation
-
-author = "Mitch"
-
-# fstring is the most straightforward way to concatenate strings
-print(f"read book by {author}.")
-
---
+++ Projects/roosevelt_madlibs.py
@@ -0,0 +1,21 @@
+# Project idea by Kylie Ying on freeCodeCamp: https://www.youtube.com/watch?v=8ext9G7xspg
+
+# Note: I only used her tutorial as a starting point for this assignment. I made it my own.
+
+# Here is a famous quote by Winston Churchill
+    # "The only Limit to our realization of tomorrow will be our doubts of today." - Franklin D. Roosevelt
+
+#string concatenation
+
+author = "Franklin D. Roosevelt"
+funnyAuthor = "Dranklin R. Foosevelt"
+noun1 = input{"Noun 1: "}
+noun2 = input{"Noun 2: "}
+noun3 = input{"Noun 3: "}
+noun4 = input{"Noun 4: "}
+
+
+madlib = f"The only Limit to our {noun1} of {noun2} will be our {noun3} of {noun4}. - {funnyAuthor} "
+
+
+#then randomise the order of the nouns so that they appear in a funny way
