23e3ca9c477e88b83c7542d2a7bd9c5388c9e112
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-10 20:16:59 +0800

    Wrap more tests in try catch block

--- Challenges/calculate_fib_number.py
+++ Challenges/calculate_fib_number.py
@@ -8,3 +8,28 @@ def fib(n):
 user_input = int(input("Which Fibonacci Number would you like to calculate? "))
 result = fib(user_input)
 print(f"The {user_input}th Fibonacci number is: {result}")
+
+
+# Test edge cases
+
+def test_fibonacci_calculator():
+    try:
+        # Base cases
+        assert fib(0) == 0
+        assert fib(1) == 1
+        
+        # Small positive integers
+        assert fib(2) == 1
+        assert fib(3) == 2
+        assert fib(4) == 3
+        
+        # Negative integers
+        
+        assert fib(-1) == -1
+        assert fib(-2) == -2
+        
+        print("All tests passed!")
+    except AssertionError as e:
+        print("Assertion Error", e, "Tests failed")
+    
+test_fibonacci_calculator()
