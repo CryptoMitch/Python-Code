3766578439611a4290a28bcbad5cf3f47fa43414
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-10 18:33:33 +0800

    Progress on Fib Calculation

--- Challenges/calculate_fib_number.py
+++ Challenges/calculate_fib_number.py
@@ -1,4 +1,8 @@
 # Write some code to calculate the 123rd Fibonacci Number
 
 def fib(n):
-    pass
+    if n < 2:
+        return("Please go higher than 1 to begin the fib calculation")
+    return fib(n-2) + fib(n-1)
+        
+print(input(n))
