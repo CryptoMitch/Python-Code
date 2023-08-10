8fc6fc10567b3ec2d8913fa392be6e008938d5a8
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-10 20:45:13 +0800

    Add time tracking to the algorithm

--- Algorithms/linear_search.py
+++ Algorithms/linear_search.py
@@ -5,6 +5,9 @@
 # Sequentially checks each element in a list until a match is found 
 # or the entire list has been searched.
 
+import time
+
+# The Algorithm
 def linear_search(arr, target):
     index = 0
     while index < len(arr):
@@ -13,8 +16,19 @@ def linear_search(arr, target):
         index += 1
     return -1 
 
-my_list = [1, 5, 8, 9, 12, 15, 18, 23, 55, 74, 81, 90, 111, 154, 186, 200]
+# The list + target
+my_list = [1, 5, 8, 9, 12, 15, 18, 23, 55, 74, 81, 90, 99, 111, 154, 186, 200]
+target = 99
 
-result = linear_search(my_list, 99)
+# Track the time taken
+start_time = time.time() # Store start time in a variable
+# Execute the function
+result = linear_search(my_list, 99) 
+end_time = time.time() # Store end time in a variable
 
-print(result)
+# Calculate the elapsed time
+elapsed_time = end_time - start_time
+print(f'Linear search took {elapsed_time: .6f} seconds')
+
+
+print(f'Index location: {result}')
