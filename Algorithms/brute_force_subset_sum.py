ae08229ce9fac2ff5304bbb2c60c1707c9b3cbe9
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-13 06:53:26 +0800

    Add new Algorithm types to solve

---
+++ Algorithms/brute_force_subset_sum.py
@@ -0,0 +1,8 @@
+# Brute Force Subset Sum
+
+# Exponential time
+# O(2^n) time complexity
+# Slow and not to be used
+
+# Given a set of integers and a target value, /
+# this algorithm checks all possible subsets of the set to determine if any subset's sum equals the target value. The number of subsets grows exponentially with the input size.
