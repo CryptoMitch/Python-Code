944460cac48de2cc5c9ebbab0e375b6c969ef400
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-12 18:00:30 +0800

    Prep print before adding functionality

--- Algorithms/bubble_sort.py
+++ Algorithms/bubble_sort.py
@@ -1,3 +1,17 @@
-# Bubble Sort Algorithm
+# Bubble Sort Algorithm (Educational Purposes Only)
 
 # steps through the list, compares adjacent elements, and swaps them if they're in the wrong order.
+
+# Time Complexity: O(n^2) - "Quadratic"
+# Space Complexity: O(1)
+# Really slow and not useful for production environments
+
+def bubble_sort(arr):
+    n = len(arr)
+    
+    
+    
+arr = [63, 4, 33, 56, 86, 334, 754, 32, 2, 3, 54, 2, 23]
+
+bubble_sort(arr)
+print("Sorted Array Results: ", arr)
