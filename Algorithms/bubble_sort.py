39dc2eb28210f8c3a97170cdc229cbeb50210d09
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-12 18:04:57 +0800

    Bubble sort functionality finalised

--- Algorithms/bubble_sort.py
+++ Algorithms/bubble_sort.py
@@ -9,7 +9,10 @@
 def bubble_sort(arr):
     n = len(arr)
     
-    
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     
 arr = [63, 4, 33, 56, 86, 334, 754, 32, 2, 3, 54, 2, 23]
 
