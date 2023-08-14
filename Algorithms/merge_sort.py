6690a9cccf93d77f7325a29e3a0a81b5f9b90efc
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-14 17:47:13 +0800

    Prep Merge Sort Challenge

---
+++ Algorithms/merge_sort.py
@@ -0,0 +1,14 @@
+# Merge Sort Algorithm
+
+# Summary: Divide-and-conquer sorting algorithm. Break down lists into sublists and merge those lists back in
+
+# Time Complexity: O(n log n)
+
+# Space Complexity: O(n) 
+
+
+#Steps 
+    # Divide
+    # Conquer
+    # Merge
+    
