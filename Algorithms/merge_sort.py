8d8bb3b722872a7b28bfc97d3b0fa3fc4f7979c8
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-14 18:02:47 +0800

    Begin work on conquer part of merge sort

--- Algorithms/merge_sort.py
+++ Algorithms/merge_sort.py
@@ -12,8 +12,8 @@
     # Conquer
     # Merge
 
-
 def merge_sort(arr):
+    # DIVIDE - Create Sublists
     # if the length of the array is greater than 1
     if len(arr) > 1:
         # Find the middle
@@ -22,3 +22,15 @@ def merge_sort(arr):
         left_half = arr[:mid] # Use slice notation to create left sublist
         right_half = arr[mid:] # Use slice notation to create right sublist
         
+    # Recursively call the function to sort both lists
+    # Run the algorithm until the sublsits are considered sorted
+    merge_sort(left_half)
+    merge_sort(right_half)
+    
+    # CONQUER - Merge the two lists
+    
+    # Initialise three viables to keep track of the positions
+    left_index = 0
+    right_index = 0
+    merged_index = 0
+    
