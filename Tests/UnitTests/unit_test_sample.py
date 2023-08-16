b01093da5e5a2dc7ea9c3f054833636db00c59f0
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-16 09:48:20 +0800

    progress on sample test

--- Tests/UnitTests/unit_test_sample.py
+++ Tests/UnitTests/unit_test_sample.py
@@ -1,3 +1,11 @@
-import unittest
+import unittest # Pythons built-in testing framework
 
-class MyTest (unittest.TestCase):
+class MyTest (unittest.TestCase): # Create individual test cases
+# Use the test_ naming convention to show each test method
+    def test_addition(self) # Self calls the instance of the class
+    # Assertions check whether we get the expected results
+    result = 2 + 2
+    self.assertEqual(result, 4) # Use assertion to verify the actual results match expected results
+    
+    
+    
