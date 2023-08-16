5e45d6ff00348b01f0a4d960462d1ece86df493f
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-16 12:06:56 +0800

    Make progress on Goldilocks project

---
+++ Projects/goldilocks.py
@@ -0,0 +1,30 @@
+# Goldilocks and the Three Bears Simulation
+
+# A fun python challenge created by Mitch Hamilton
+
+# This Python script simulates the famous fairy tale "Goldilocks and the Three Bears."
+# Goldilocks visits the bears' house and tries their porridge and beds.
+# The bears' characteristics include their names, porridge temperature, and bed softness.
+# Goldilocks provides her preferred porridge temperature and bed softness.
+# The script then displays humorous responses based on whether Goldilocks likes the porridge and beds.
+# Enjoy the playful interactions between Goldilocks and Baby Bear, Mama Bear, and Papa Bear!
+
+#low level logic: class definitions, methods
+class Bear: # Superclass that is shared by all bears in the story
+    def __init__(self, name, porride_temp, bed_softness): # Constructor, automatically called when creating a new instance. It sets up the initial state of the object.
+        self.name = name
+        self.porride_temp = porride_temp
+        self.bed_softness = bed_softness
+        
+
+def eat_porridge(self, porridge_temp):
+    pass
+
+
+
+# Entry Point
+# High-level logic: Create instances of the Bear class
+def main():
+    baby_bear("Baby Bear", 30, 5)
+    mama_bear("Baby Bear", 40, 8)
+    papa_bear("Papa Bear", 50, 10)
