8f67327091c2f8cfd95bcf35089013a8dfc89573
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-16 10:08:42 +0800

    Intentionally Test Failing Cases

--- Tests/UnitTests/unit_test_sample.py
+++ Tests/UnitTests/unit_test_sample.py
@@ -6,20 +6,33 @@ class MyTest (unittest.TestCase): # Create individual test cases
         # Assertions check whether we get the expected results
         result = 9 + 2
         self.assertEqual(result, 11) # Use assertion to verify the actual results match expected results
+        print(f'Addition result of {result} is correct')
     
     def test_subtraction(self):
         result = 2 - 1
         self.assertEqual(result, 1)
+        print(f'Subtraction result of {result} is correct')
         
     def test_multiplication(self):
         result = 3 * 4
         self.assertEqual(result, 12)
+        print(f'Multiplication result of {result} is correct')
         
     def test_modulo(self):
-        result = 4 % 2 == 0
-        self.assertEqual(result, True)
+        result = 4 % 2
+        self.assertTrue(result == 0)
+        print(f'Modulo result of {result} is correct')
         
+# Intentionally Failing Test Cases
+    def test_addition_fail(self):
+        result = 1 + 1
+        self.assertEqual(result, 3) # Intentional Failing Test Case
+        
+    def test_subtraction_fail(self):
+        result = 3 - 2
+        self.assertEqual(result, 4) # Intentional Failing Test Case
 
+        
 # Use built-in variable to check the current module is being run as the main module 
 if __name__ == '__main__':
     unittest.main()
