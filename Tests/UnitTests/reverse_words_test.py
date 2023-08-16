281bcfe5d9956f5abce9c73d3b84bcc972af6572
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-16 11:42:16 +0800

    Troubleshoot file directory naming

--- Tests/UnitTests/reverse_words_test.py
+++ Tests/UnitTests/reverse_words_test.py
@@ -1,9 +1,32 @@
 import unittest
+import sys
+print(sys.path)
+import os
+
+
+print("Current directory:", os.getcwd())
+print("Module search paths:", sys.path)
+
+# Add the 'Challenges' folder to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 from Challenges.reverse_words import reverse_words # Use the module names without the file extension to pull in the reverse words code
 
 class TestReverseWords(unittest.TestCase): # Define a blueprint class that inherits a set of methods
-    
-    
+    def test_example(self):
+        self.assertEqual(reverse_words("Mitch Loves Code"),"Code Loves Mitch")
+
+    def test_empty_string(self):
+        self.assertEqual(reverse_words(""), "")
+        
+    def test_single_word(self):
+        self.assertEqual(reverse_words("Code"), "Code")
+        
+    def test_multiple_spaces(self):
+        self.assertEqual(reverse_words("Hello  my  name  is  "), "  is  name  my  Hello")
     
     
 # Create a test suite
+
+if __name__ == '__main__':
+    unittest.main()
