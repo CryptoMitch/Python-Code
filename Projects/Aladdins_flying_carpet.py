d012a8b7f16c9cc2bc4d21f06d5ac0daa3bfc9b5
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-17 08:12:22 +0800

    Begin Aladdin's Flying Carpet challenge

--- Projects/Aladdins_flying_carpet.py
+++ Projects/Aladdins_flying_carpet.py
@@ -3,4 +3,13 @@
 # Design a flying carpet that can move in different directions 
 # based on Aladdin(the User)'s commands
 
-# Skills: 
+# Skills: List Comprehension, Decorators, Context Manager, Magic Methods, Async Await
+
+class FlyCarpet():
+    def __init__(self):
+        self.position = [0, 0]
+        
+    
+    
+        
+        
