62c0349ecca6e4f7a583df9a80b19afe10f4c64b
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-17 09:56:16 +0800

    complete save_to_file method

--- Projects/contactbook.py
+++ Projects/contactbook.py
@@ -1,6 +1,6 @@
 # Contact Book Project
 
-# Skills Demonstrated: Object Oriented Programming, Encapsulation, Dictionaries, .get() method, Lists, File handling, Input Handling, Control Structures, Strings, Exception handling
+# Skills Demonstrated: Object Oriented Programming, Encapsulation, Dictionaries, .get() method, Lists, File handling, Input Handling I/O, Control Structures, Strings, Exception handling
 
 class Profile:
     def __init__(self, username, phone_number): # Constructor that initialises the attributes of the profile instance
@@ -23,9 +23,11 @@ class ContactBook: # Blueprint for how the instances will behave
     def list_profiles(self): # List all the profiles in the ContactBook
         return list[self.profiles.values()] # Retrieve the values from the profiles dictionary, convert to list, return the list
     
-    def save_to_file(self):
-        pass
-    
+    def save_to_file(self, filename): # Responsible for saving the profiles stored in the contact book to a file
+        with open(filename, 'w') as file: # Use context manager to open the file in writing mode, ensure it closes afterwards. Assign to variable file.
+            for profile in self.profiles.values(): # Iterate through the values of the profiles dictionary
+                file.write(f"{profile.username}, {profile.phone_number}\n") # Write usernames and phone numbers to the file
+                # By leaving this code indent, the file will close because of the context manager 'with' statement
     def load_from_file(self):
         pass
     
