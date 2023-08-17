61dcd59ce7e41db8c8ec32f5592e9289cdef9f4d
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-17 09:27:41 +0800

    Complete add_profile method

--- Projects/contactbook.py
+++ Projects/contactbook.py
@@ -1,6 +1,6 @@
 # Contact Book Project
 
-# Skills Demonstrated: Object Oriented Programming, File handling, Input Handling, Control Structures, Strings, Dictionaries, Exception handling
+# Skills Demonstrated: Object Oriented Programming, Encapsulation, File handling, Input Handling, Control Structures, Strings, Dictionaries, Exception handling
 
 class Profile:
     def __init__(self, username, phone_number): # Constructor that initialises the attributes of the profile instance
@@ -12,12 +12,12 @@ class Profile:
 
 class ContactBook: # Blueprint for how the instances will behave
     def __init__(self, Profile): # Constructor to initialise the state of the instance
-        self.profiles = {} 
+        self.profiles = {} # Empty dictionary of profile instances
         
-    def add_profile(self, profile): # Method to add a profile to the ContactBook
-        pass
+    def add_profile(self, profile): # Method to add a profile to the ContactBook, taking two parameters
+        self.profiles[profile.username] = profile # Perform the action of adding the profile to the contact book
         
-    def search_profile(): # Method that returns a list of all the profiles
+    def search_profile(self, ): # Method that returns a list of all the profiles
         pass
     
     def list_profiles(self):
@@ -25,5 +25,11 @@ class ContactBook: # Blueprint for how the instances will behave
     
     def save_to_file(self):
         pass
+    
+    def load_from_file(self):
+        pass
+    
+    
+
     
     
