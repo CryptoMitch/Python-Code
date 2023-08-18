56ba288bcac6659897d9be92918bf39109525483
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-18 15:21:56 +0800

    Add progress and research on how to improve it

--- Algorithms/quick_sort.py
+++ Algorithms/quick_sort.py
@@ -8,3 +8,24 @@
 
 # Space Complexity: O(n log (n)) - quite efficient for memory usage because of the recursion stack
 
+def quick_sort(arr):
+    # Base case: return array if 1 or less elements are inside the array
+    if len(arr) <= 1:
+        return arr 
+    
+    # Choose a pivot
+    pivot = arr[len(arr) // 2] # Pick the middle element of the array. Use Square brackets to access the elements in the array using their index positions
+        # Please note that using the integer division above can cause issues with picking an unbalanced pivot because integer division rounds down to zero rather than a decimal
+        
+        
+        
+        
+        
+        
+# Ways to improve this algorithm: 
+    # 1. Randomized pivot
+    # 2. Choose the middle of three randomly selected elements as the pivot
+    # 3. Median of Medians: Select groups of elements, find their median and then choose the median of the medians as the pivot
+    # 4. Hybrid Approach: Inside Quicksort switch to Insertion Sort for smaller subarrays
+    
+    
