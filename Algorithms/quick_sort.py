3e064e2c412033b7999de1c387f362c1c8617476
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-18 15:11:36 +0800

    Begin Quick Sort Algorithm

---
+++ Algorithms/quick_sort.py
@@ -0,0 +1,10 @@
+# Quick Sort Algorithm (Often used)
+
+# Divide and Conquer Algorithm using Pivots and partitioning into two sub arrays
+
+# Best Time Complexity: O(n log (n)) - when dividing into equal halves
+# Average Time Complexity: O(n log (n)) 
+# Worst Time Complexity: O(O n^2) - when a pivot is chosen poorly and creates unbalanced subarrays
+
+# Space Complexity: O(n log (n)) - quite efficient for memory usage because of the recursion stack
+
