2028bbf2013b0d91fa1a663242e08e48b90356e6
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-19 10:04:40 +0800

    Begin BFS Algorithm Pseudocode

---
+++ Algorithms/breadth_first_search.py
@@ -0,0 +1,25 @@
+# Breadth First Search (BFS) Algorithm
+
+# Traverse or search through data structures like graphs or trees level by level.
+
+
+
+
+# Start at the root(AKA arbitrary or inital) node in a graph
+
+
+
+# Explore neighbours
+
+
+
+# Move to the next level of the queue
+
+
+    # Do this until the queue is empty
+
+
+
+
+
+# Adjacency list
