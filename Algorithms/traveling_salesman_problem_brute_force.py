62c73ddb707b64fad772aac2aced610e3b372d5f
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-25 16:54:39 +0800

    Begin Traveling Salesman Problem

--- Algorithms/traveling_salesman_problem_brute_force.py
+++ Algorithms/traveling_salesman_problem_brute_force.py
@@ -1,4 +1,16 @@
 # Traveling Salesman Problem (TSP) Brute Force: 
 # Time Complexity: O(n!)
 
+# Do not use in production code as it has factorial time complexity which is one of the least efficient.
+
+# For example, with just 10 cities, you'd need to perform 3,628,800 operations. 
+# With 20 cities, you'd need to perform 2,432,902,008,176,640,000 operations, which is a staggering number.
+
+# The Traveling Salesman Problem is a classic optimization problem. 
+# It involves finding the shortest possible route that visits a set of cities 
+# and returns to the starting city. The brute-force solution involves checking 
+# all possible permutations of cities to find the shortest route.
+
 # n is the number of cities. The algorithm checks all possible permutations of cities
+
+import itertools # tools for working with iterators, combinations, and permutations.
