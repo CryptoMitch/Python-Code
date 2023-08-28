3b83e39d66321ddf07cb2be8686450e2b66f983f
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-28 16:55:29 +0800

    Euclidean Algorithm usecases

--- Algorithms/euclidean_algorithm.py
+++ Algorithms/euclidean_algorithm.py
@@ -8,3 +8,16 @@ num1 = 36
 num2 = 30
 gcd = euclidean_algorithm(num1, num2)
 print(f"The GCD of {num1} and {num2} is {gcd}")
+
+
+
+# Use it when you need to 
+
+    # Simplify fractions.
+    # Compute modular inverses.
+    # Solve Diophantine equations.
+    # Check coprimality.
+    # Generate random numbers.
+    # Optimize algorithms.
+    # Detect and correct errors.
+    # Create music patterns.
