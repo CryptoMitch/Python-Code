5be8fed65e7d568c00da03bb807d99cc81173729
Mitch Hamilton <thecryptomitch@gmail.com>
2023-09-01 08:24:19 +0800

    Refactor Leetcode 121

--- LeetCodeProblems/121_Best_Time_To_Buy_And_Sell_Stock.py
+++ LeetCodeProblems/121_Best_Time_To_Buy_And_Sell_Stock.py
@@ -5,33 +5,23 @@
 # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
-from typing import List
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        lowest_day, highest_day, profit = 0, 0, 0
+        lowest_price = prices[0] 
+        max_profit = 0
 
-        for i in range(len(prices)):
-            if prices[i] < prices[lowest_day]:
-                lowest_day = i
-            elif prices[i] - prices[lowest_day] > profit:
-                highest_day = i
-                profit = prices[highest_day] - prices[lowest_day]
-
-        if highest_day > lowest_day:
-            print(f"The best time to buy is day {lowest_day + 1} and sell is day {highest_day + 1} .")
-            print(f"Total profit is: {profit}")
-            return profit
-        else:
-            print("No profitable transaction.")
+        if not prices: # Check if list is empty
             return 0
 
-# Example usage
-solution = Solution()
-prices = [7, 1, 5, 3, 6, 4]
-print(solution.maxProfit(prices))
+        for price in prices:
+            if price < lowest_price:
+                lowest_price = price
+            elif price - lowest_price > max_profit:
+                max_profit = price - lowest_price
 
-# Output given: 
-# The best time to buy is day 2 and sell is day 5 .
-# Total profit is: 5
-# 5
+        return max_profit
+
+
+
+
+
