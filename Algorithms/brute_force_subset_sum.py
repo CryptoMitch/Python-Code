1e6e427ea7a626b78cf56826399e1f9fffcbc1ed
Mitch Hamilton <thecryptomitch@gmail.com>
2023-09-02 09:42:47 +0800

    LeetCode 345 Completed

--- Algorithms/brute_force_subset_sum.py
+++ Algorithms/brute_force_subset_sum.py
@@ -4,5 +4,5 @@
 # O(2^n) time complexity
 # Slow and not to be used
 
-# Given a set of integers and a target value, /
+# Given a set of integers and a target value,
 # this algorithm checks all possible subsets of the set to determine if any subset's sum equals the target value. The number of subsets grows exponentially with the input size.
