8d44abf7b84f4add30fe97106c868484b79abafd
Mitch Hamilton <thecryptomitch@gmail.com>
2023-09-05 19:41:40 +0800

    Add comments

--- LeetCodeProblems/1323_maximum_69_number.py
+++ LeetCodeProblems/1323_maximum_69_number.py
@@ -1,6 +1,6 @@
-You are given a positive integer num consisting only of digits 6 and 9.
+# You are given a positive integer num consisting only of digits 6 and 9.
 
-Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+# Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
 
 # Built in functions
 class Solution:
