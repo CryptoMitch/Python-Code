4967bd0835fbc2c0d6da1edd0e3bcde0008b49c1
Mitch Hamilton <thecryptomitch@gmail.com>
2023-09-07 18:42:25 +0800

    Swap back to logical expression to improve runtime

--- LeetCodeProblems/2652_Sum_Multiples.py
+++ LeetCodeProblems/2652_Sum_Multiples.py
@@ -12,7 +12,7 @@ class Solution:
         sum_total = 0
 
         for num in range(1, n+1):
-            if num % 3 == 0 or num % 5 == 0 or num % 7 == 0:
+            if num % 3 == 0 or num % 5 == 0 or num % 7 == 0: # faster than generator expression
                 sum_total += num
         
         return sum_total
