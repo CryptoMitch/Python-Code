6403f40924a4639fca20a82826f34d5c1099857a
Mitch Hamilton <thecryptomitch@gmail.com>
2023-09-07 18:35:09 +0800

    LeetCode 2652 Completed

---
+++ LeetCodeProblems/2652_Sum_Multiples.py
@@ -0,0 +1,18 @@
+# LeetCode 2652: Sum Multiples
+
+# Given a positive integer n, 
+# find the sum of all integers in the range [1, n] 
+# inclusive that are divisible by 3, 5, or 7.
+
+# Return an integer denoting the sum of all numbers in 
+# the given range satisfying the constraint.
+
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        sum_total = 0
+
+        for num in range(1, n+1):
+            if num % 3 == 0 or num % 5 == 0 or num % 7 == 0:
+                sum_total += num
+        
+        return sum_total
