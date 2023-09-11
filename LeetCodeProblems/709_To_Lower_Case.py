180bc467299c9b6a796b750526230fa4e08eea86
Mitch Hamilton <thecryptomitch@gmail.com>
2023-09-12 07:39:36 +0800

    Complete Leetcode 709

---
+++ LeetCodeProblems/709_To_Lower_Case.py
@@ -0,0 +1,10 @@
+# 709. To Lower Case
+
+# Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+# This code is unnecessarily easy. I will refactor in the future to take up less memory.
+
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return (s.lower())
